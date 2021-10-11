0ec4cc9e1984db5cf67b093db89f33392eadbb3e
Paajonen <72916863+Paajonen@users.noreply.github.com>
2021-10-11 17:07:22 +0300

    Tehtävät pilveen

---
+++ formtesti.js
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Workshop 1</title>
+		<!-- script tag is where Javascript goes to -->
+		<script>
+
+
+			
+		</script>
+	
+	</head>
+
+	<body>
+    <h1>Please fill in your data</h>
+        <form name="myForm" action="#" onsubmit="return validateForm()" method="post">
+        Firstname: <input type="text" name
+
+	
+	</body>
+
+</html>
